35e831c4bc32434236e060e44c4ca9863cffceb8
marcelli-lima <marcelli_lima@hotmail.com>
2022-06-30 10:57:15 -0300

    funçao para calcular area de um triangulo

--- isd-011-project-playground-functions/src/challenges.js
+++ isd-011-project-playground-functions/src/challenges.js
@@ -1,2 +1,10 @@
+// Desafio 1
 function compareTrue(bolean, bolean2) {return bolean && bolean2 ? "true" : "false"}
 console.log(compareTrue(false, true));
+
+// Desafio 2
+function calcArea(base, height) {
+  let area = ((base * height) / 2)
+  return area;
+}
+console.log(calcArea(10, 50));
