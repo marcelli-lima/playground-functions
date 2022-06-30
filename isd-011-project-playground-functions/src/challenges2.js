a0af7053b8210833158c9c5399c2e455112f47ce
marcelli-lima <marcelli_lima@hotmail.com>
2022-06-30 11:07:15 -0300

    funçao de numero de telefone

--- isd-011-project-playground-functions/src/challenges2.js
+++ isd-011-project-playground-functions/src/challenges2.js
@@ -18,9 +18,50 @@ console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"],
   "Marcelli"));
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function threeSameNumbers(array) {
+  let conferindo = array;
+  let contador = 0;
+  for (let index of array) {
+    for (let confIndex of conferindo) {
+      if (index === confIndex) {
+        contador += 1;
+      }
+      if (contador >= 3) {
+        return contador;
+      }
+    }
+    contador = 0;
+  }
+  return contador;
 }
+
+function generatePhoneNumber(array) {
+ 
+  if (array.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  for (let index of array) {
+    if (index < 0 || index > 9 || threeSameNumbers(array) === 3) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+  let telefone = '(xx) xxxxx-xxxx';
+  let telFinal = '';
+  for (let telIndex in telefone) {
+    for (let arIndex of array) {
+      if (telefone[telIndex] === 'x') {
+        telFinal += arIndex;
+        array.shift();
+        break;
+      } else {
+        telFinal += telefone[telIndex];
+        break;
+      }
+    }
+  }
+  return telFinal;
+}
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 7, 6, 5]));
 
 // Desafio 12
 function triangleCheck() {
