e7e72e35ee1931400dced20e1fbfb4d33726753d
marcelli-lima <marcelli_lima@hotmail.com>
2022-06-30 11:05:00 -0300

    desafio tech list

--- isd-011-project-playground-functions/src/challenges2.js
+++ isd-011-project-playground-functions/src/challenges2.js
@@ -1,7 +1,21 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(array, name) {
+  if (array.length === 0) {
+    return "Vazio!"
+  }
+  let list = [];
+  array.sort();
+  for (let i = 0; i < array.length; i++) {
+    let object = {
+      tech: array[i],
+      name: name
+    }
+    list.push(object)
+  }
+  return list;
 }
+console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"],
+  "Marcelli"));
 
 // Desafio 11
 function generatePhoneNumber() {
