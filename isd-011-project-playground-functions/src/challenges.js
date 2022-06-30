aee6e10c13c323243a2e1894928671a41a7ce5e9
marcelli-lima <marcelli_lima@hotmail.com>
2022-06-30 10:55:40 -0300

    melhoria do desafio 1

--- isd-011-project-playground-functions/src/challenges.js
+++ isd-011-project-playground-functions/src/challenges.js
@@ -1,8 +1,2 @@
-function compareTrue(bolean, bolean2) {
-  if (bolean === true && bolean2 === true) {
-    return true;
-  } else {
-    return false;
-  }
-}
-console.log(compareTrue(true, true));
+function compareTrue(bolean, bolean2) {return bolean && bolean2 ? "true" : "false"}
+console.log(compareTrue(false, true));
